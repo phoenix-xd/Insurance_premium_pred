6d5e4e0c2f365fdd8d65d1db7f64c64f0b9a6cb0
phoenix-xd <sarthakbansal131@gmail.com>
2024-09-24 03:04:19 +0530

    frontend update

--- application.py
+++ application.py
@@ -7,15 +7,28 @@ app = application
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('home.html')
+
 @app.route('/home')
 def document():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/features')
+def features():
+    return render_template('features.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/predict', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
-        return render_template('form.html')
+        return render_template('predict.html')
     else:
         # Retrieve form data
         data = CustomData(
@@ -24,7 +37,7 @@ def predict_datapoint():
             children=int(request.form.get('children')),
             sex=request.form.get('sex'),
             smoker=request.form.get('smoker'),
-            region=request.form.get('region_categories')  # Match with the form name
+            region=request.form.get('region')  # Match with the form name
         )
         
         # Convert form data to DataFrame
@@ -37,7 +50,7 @@ def predict_datapoint():
         # If `pred` is an array or list, round the first element
         results = round(pred[0]) if isinstance(pred, (list, np.ndarray)) else round(pred)
         
-        return render_template('form.html', final_result=results)
+        return render_template('prediction.html', final_result=results , age=data.age , bmi=data.bmi ,children=data.children, sex=data.sex, smoker= data.smoker, region= data.region)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
